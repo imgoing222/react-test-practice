c4b33a7a58878b5a51ab62232e7509148f0b08bd
9603100 <imgoing222@gmail.com>
2022-07-07 22:50:06 +0900

    feat: 서버에서 데이터 가져올 때 에러 발생 시 배너 생성 및 테스트 코드 작성

--- react-shop-test/src/pages/OrderPage/Type.jsx
+++ react-shop-test/src/pages/OrderPage/Type.jsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import ErrorBanner from "../../components/ErrorBanner";
 import Products from "./Products";
 
 function Type({ orderType }) {
 	const [items, setItems] = useState([]);
+	const [error, setError] = useState(false);
 
 	useEffect(() => {
 		loadItems(orderType);
@@ -14,10 +16,13 @@ function Type({ orderType }) {
 			let response = await axios.get(`http://localhost:5000/${orderType}`);
 			setItems(response.data);
 		} catch (error) {
-			console.error(error);
+			setError(true);
 		}
 	};
 
+	if (error) {
+		return <ErrorBanner message="에러가 발생했습니다." />;
+	}
 	const ItemComponents = orderType === "products" ? Products : null;
 
 	const optionItems = items.map((item) => (
