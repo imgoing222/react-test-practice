e444615ad42c053090c91994fab61788e2b7f3bf
9603100 <imgoing222@gmail.com>
2022-07-07 23:32:31 +0900

    design: UI 수정 및 추가

---
+++ react-shop-test/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import OrderPage from "./pages/OrderPage/OrderPage";
+
+function App() {
+	return (
+		<div style={{ padding: "4rem" }}>
+			<OrderPage />
+		</div>
+	);
+}
+
+export default App;
