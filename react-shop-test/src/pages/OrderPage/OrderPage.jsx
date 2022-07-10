5f42c93b9fc6099b1226cfe6f771148835af5a7e
9603100 <imgoing222@gmail.com>
2022-07-11 02:38:01 +0900

    feat: 페이지 별 step 기능 추가

--- react-shop-test/src/pages/OrderPage/OrderPage.jsx
+++ react-shop-test/src/pages/OrderPage/OrderPage.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import { OrderContext } from "../../contexts/OrderContext";
 import Type from "./Type";
 
-function OrderPage() {
+function OrderPage({ setStep }) {
+	const [orderDatas] = useContext(OrderContext);
+
 	return (
 		<div>
 			<h1>Travel Products</h1>
@@ -13,9 +16,9 @@ function OrderPage() {
 					<Type orderType="options" />
 				</div>
 				<div>
-					<h2>Total Price: </h2>
+					<h2>Total Price: {orderDatas.totals.total}</h2>
 					<br />
-					<button>주문</button>
+					<button onClick={() => setStep(1)}>주문하기</button>
 				</div>
 			</div>
 		</div>
